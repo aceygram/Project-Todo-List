dd3df884f862e1d366d831435e7f65224f8fdcbe
Promise Okeke <ace4facebook@gmail.com>
2023-11-21 20:32:16 +0100

    for iphone 6 screen

--- main.js
+++ main.js
@@ -454,7 +454,6 @@ addProjectButton.addEventListener('click', () => {
 
     // Append the input element to the middle menu
     sideMenu.appendChild(inputElement);
-    inputElement.focus();
     isInputVisible = true; // Set the flag to indicate that the input is visibley
   }
 });
